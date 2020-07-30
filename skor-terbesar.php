94a6d3b96c3aadc2972df49f3060a7b686931ec4
Alif <yuniarenggaresita.ye@gmail.com>
2020-07-30 09:10:46 +0700

    php-3

--- skor-terbesar.php
+++ skor-terbesar.php
@@ -1,6 +1,46 @@
 <?php
 function skor_terbesar($arr){
 //kode di sini
+  $dim = count($arr);
+    $laravel = 0;
+    $reactN = 0;
+    $reactJ = 0;
+    for($i=0;$i<$dim;$i++)
+    {
+      if($arr[$i]['kelas'] == 'Laravel')
+        {
+          if($arr[$i]['nilai']>$laravel)
+            {
+              $laravel = $arr[$i]['nilai'];
+                $larName = $arr[$i]['nama'];
+                $larKelas = $arr[$i]['kelas'];
+            }
+        }
+        else if($arr[$i]['kelas'] == 'React Native')
+        {
+          if($arr[$i]['nilai']>$reactN)
+            {
+              $reactN = $arr[$i]['nilai'];
+                $reacNName = $arr[$i]['nama'];
+                $reacNKelas = $arr[$i]['kelas'];
+            }
+        }
+        else
+        {
+          if($arr[$i]['nilai']>$reactJ)
+            {
+              $reactJ = $arr[$i]['nilai'];
+                $reacJName = $arr[$i]['nama'];
+                $reacJKelas = $arr[$i]['kelas'];
+            }
+        }
+    }
+    $laravel = array("nama"=>$larName, "kelas"=>$larKelas, "nilai"=>$laravel);
+    $reactNative = array("nama"=>$reactNName, "kelas"=>$reactNKelas, "nilai"=>$reactN);
+    $reactJS = array("nama"=>$reactJName, "kelas"=>$reactJKelas, "nilai"=>$reactJ);
+    $hasil = array($laravel, $reactNative, $reactJS);
+    
+  return $hasil;
 }
 
 // TEST CASES
